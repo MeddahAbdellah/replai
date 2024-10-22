8d5e605c31e70428d1d1a020c010356338f54f22
MeddahAbdellah <meddah.abdellah.spcx@gmail.com>
2024-10-22 10:52:24 +0200

    🔧 fix amqp publisher typing

--- runner/processors/amqp-publisher.ts
+++ runner/processors/amqp-publisher.ts
@@ -8,7 +8,7 @@ export async function amqpPublisher(config: {
     runId: string;
     messages: Omit<Message, "id" | "runId">[];
     toolsOnly: boolean;
-  }) => void
+  }) => Promise<void>
 > {
   const { amqpUrl, queue } = config;
   const connection = await amqp.connect(amqpUrl);
